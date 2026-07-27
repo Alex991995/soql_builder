50b0729e7c5e39f06f45c1989e9ebc89b803bb78
Alex <aleksandr.kisel@vrpconsulting.com>
2026-07-27 17:56:04 +0300

    feat: add 3 selectors and input

--- force-app/main/default/lwc/soqlBuilder/soqlBuilder.js
+++ force-app/main/default/lwc/soqlBuilder/soqlBuilder.js
@@ -2,9 +2,21 @@ import { LightningElement, wire } from "lwc";
 import getNameAllObjects from "@salesforce/apex/ObjectManager.getNameAllObjects";
 import getNameFieldsOfObject from "@salesforce/apex/ObjectManager.getNameFieldsOfObject";
 
+const sortingOrder = {
+  ASC: "ASC",
+  DESC: "DESC"
+};
+
 export default class Main extends LightningElement {
   @wire(getNameAllObjects) nameAllObjects;
   selectObjName = "";
+  selectFieldName = "";
+  selectSortingField = "";
+
+  sortingStrategy = [
+    { label: "A to Z", value: sortingOrder.ASC },
+    { label: "Z to A", value: sortingOrder.DESC }
+  ];
 
   @wire(getNameFieldsOfObject, { objectName: "$selectObjName" })
   nameFields;
@@ -12,7 +24,7 @@ export default class Main extends LightningElement {
   /**
    * @description: Create list objects for lightning-select with props label and value
    */
-  get nameOptions() {
+  get nameObjectsOptions() {
     let result = [];
     if (this.nameAllObjects.data) {
       this.nameAllObjects.data.forEach((element) => {
@@ -24,9 +36,44 @@ export default class Main extends LightningElement {
     return result;
   }
 
-  handleChange(event) {
+  get fieldsForSorting() {
+    let result = [];
+    if (this.nameFields.data) {
+      this.nameFields.data.forEach((element) => {
+        let option = { label: element, value: element };
+        result.push(option);
+      });
+      return result;
+    }
+    return result;
+  }
+
+  get nameFieldsOptions() {
+    let result = [];
+    if (this.nameFields.data) {
+      const countFiled = { label: "count()", value: "count()" };
+      result.push(countFiled);
+      this.nameFields.data.forEach((element) => {
+        let option = { label: element, value: element };
+        result.push(option);
+      });
+      return result;
+    }
+    return result;
+  }
+
+  handleObjName(event) {
     this.selectObjName = event.detail.value;
-    console.log("dd");
-    console.log(this.nameFields);
+    this.selectFieldName = "";
+  }
+
+  handleFieldName(event) {
+    this.selectFieldName = event.detail.value;
+    let query = `SELECT ${this.selectFieldName} FROM ${this.selectObjName}`;
+    this.template.querySelector("lightning-textarea").value = query;
+  }
+  handleQueryClick() {
+    // let query = `SELECT ${this.selectFieldName} FROM ${this.selectObjName}`;
+    // this.template.querySelector("lightning-textarea").value = query;
   }
 }
