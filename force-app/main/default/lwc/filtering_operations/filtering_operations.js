1efb4905cd6424fc8d9deaaf52cbc88a2c96dc1e
Alex <aleksandr.kisel@vrpconsulting.com>
2026-07-28 17:55:50 +0300

    feat: create filter, sorting logic

---
+++ force-app/main/default/lwc/filtering_operations/filtering_operations.js
@@ -0,0 +1,16 @@
+const filteringOperations = {
+  equal: " = ",
+  notEqual: " != ",
+  greaterThan: " > ",
+  lessThan: " < ",
+  greaterThanOrEqual: " >= ",
+  lessThanOrEqual: " <= "
+};
+
+let filteringOptions = [];
+
+for (const value of Object.values(filteringOperations)) {
+  filteringOptions.push({ label: value, value: value });
+}
+
+export { filteringOperations, filteringOptions };
--- force-app/main/default/lwc/soqlBuilder/soqlBuilder.js
+++ force-app/main/default/lwc/soqlBuilder/soqlBuilder.js
@@ -1,17 +1,26 @@
 import { LightningElement, wire } from "lwc";
 import getNameAllObjects from "@salesforce/apex/ObjectManager.getNameAllObjects";
 import getNameFieldsOfObject from "@salesforce/apex/ObjectManager.getNameFieldsOfObject";
+import { filteringOptions } from "c/filtering_operations";
 
 const sortingOrder = {
   ASC: "ASC",
   DESC: "DESC"
 };
 
-export default class Main extends LightningElement {
+export default class SoqlBuilder extends LightningElement {
   @wire(getNameAllObjects) nameAllObjects;
+  // objQuery = {
+  //   select: "",
+  //   from: "",
+  //   where: "",
+  //   orderBy: ""
+  // };
   selectObjName = "";
   selectFieldName = "";
   selectSortingField = "";
+  selectFilteringOperation = "";
+  filteringOperations = filteringOptions;
 
   sortingStrategy = [
     { label: "A to Z", value: sortingOrder.ASC },
@@ -71,9 +80,44 @@ export default class Main extends LightningElement {
     this.selectFieldName = event.detail.value;
     let query = `SELECT ${this.selectFieldName} FROM ${this.selectObjName}`;
     this.template.querySelector("lightning-textarea").value = query;
+    //  this.template.querySelector("lightning-textarea").value = this.query;
+  }
+  handleSortingField(event) {
+    this.selectSortingField = event.detail.value;
+    let query = `SELECT ${this.selectFieldName} FROM ${this.selectObjName} ORDER BY ${this.selectSortingField}`;
+    this.template.querySelector("lightning-textarea").value = query;
   }
   handleQueryClick() {
     // let query = `SELECT ${this.selectFieldName} FROM ${this.selectObjName}`;
     // this.template.querySelector("lightning-textarea").value = query;
+  }
+  handleFilteringField(event) {
+    this.selectFilteringField = event.detail.value;
+  }
+  handleFilteringOperation(event) {
+    this.selectFilteringOperation = event.detail.value;
+  }
+
+  handleFilterValueChange(event) {
+    this.selectFilterValue = event.detail.value;
+    let query = "";
+    // let query = `
+    const SELECT = "SELECT " + this.selectFieldName;
+    const FROM = "FROM " + this.selectObjName;
+    const WHERE =
+      "WHERE " +
+      this.selectFilteringField +
+      " " +
+      this.selectFilteringOperation +
+      " " +
+      this.selectFilterValue;
+
+    if (!this.selectFilterValue) {
+      query = `${SELECT} ${FROM}`;
+    } else {
+      query = `${SELECT} ${FROM} ${WHERE}`;
+    }
+
+    this.template.querySelector("lightning-textarea").value = query;
   }
 }
