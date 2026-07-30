2fa930deebfd21227457944f82bc2fe705d4e40f
Alex <sashe.1995@gmail.com>
2026-07-30 17:26:29 +0300

    feat: implemet all building soql strucutre for input ruuner

--- force-app/main/default/lwc/soqlBuilder/soqlBuilder.js
+++ force-app/main/default/lwc/soqlBuilder/soqlBuilder.js
@@ -20,7 +20,10 @@ const nullOptions = [
 
 export default class SoqlBuilder extends LightningElement {
   @wire(getNameAllObjects) nameAllObjects;
-  
+  @wire(getNameFieldsOfObject, { objectName: "$selectObjName" })
+  nameFields;
+  soqlQuery = "";
+
   selectObjName = "";
   selectFieldName = "";
   selectSortingField = "";
@@ -29,17 +32,10 @@ export default class SoqlBuilder extends LightningElement {
   selectFilterValue = "";
   selectNull = nullOptions[0].value;
   selectSortingStrategy = sortingOrder.ASC;
+
   filteringOperations = filteringOptions;
   sortingStrategy = sortingStrategy;
   nullOptions = nullOptions;
-
-  soqlQuery = "";
-  // isFieldNameSelected = !!this.selectFieldName;
-
-  isDisabled = true;
-
-  @wire(getNameFieldsOfObject, { objectName: "$selectObjName" })
-  nameFields;
 
   /**
    * @description: Create list objects for lightning-select with props label and value
@@ -104,13 +100,24 @@ export default class SoqlBuilder extends LightningElement {
     return {
       select: `SELECT ${this.selectFieldName}`,
       from: `FROM ${this.selectObjName}`,
-      where: `WHERE ${this.selectFilteringField}`,
-      operation: this.selectFilteringOperation,
-      input: this.selectFilterValue,
-      orderBy: `ORDER BY ${this.selectSortingField}`,
-      sortStrategy: `${this.selectSortingStrategy}`,
-      nullQuery: `${this.selectNull}`
+      where: this.selectFilteringField && this.selectFilterValue && 'WHERE ' + this.selectFilteringField,
+      operation: this.selectFilteringField && this.selectFilterValue && this.selectFilteringOperation,
+      input: this.selectFilteringField && this.selectFilterValue,
+      orderBy: this.selectSortingField && 'ORDER BY ' + this.selectSortingField,
+      sortStrategy: this.selectSortingField && this.selectSortingStrategy,
+      nullQuery: this.selectSortingField && this.selectNull
     };
+  }
+
+  renderedCallback() {
+    console.log("objQuery", Object.values( this.objQuery));
+
+    console.log("selectObjName", this.selectObjName);
+    console.log("selectFieldName", this.selectFieldName);
+    console.log("selectSortingField", this.selectSortingField);
+    console.log("selectFilteringField", this.selectFilteringField);
+    console.log("selectFilteringOperation", this.selectFilteringOperation);
+    console.log("selectFilterValue", this.selectFilterValue);
   }
 
   /**
@@ -133,73 +140,62 @@ export default class SoqlBuilder extends LightningElement {
 
   handleSortingField(event) {
     this.selectSortingField = event.detail.value;
-    const { select, from, orderBy, sortStrategy, nullQuery } = this.objQuery;
-    const firstPartQuery = `${select} ${from}`;
+    const { select, from, where, operation, input} = this.objQuery;
+    const firstPartQuery = `${select} ${from} ${this.selectFilteringField && where + ' ' + operation + ' ' + input}`;
 
     if (!this.selectSortingField) {
       this.soqlQuery = firstPartQuery;
       return;
     }
-    const query = `${firstPartQuery} ${orderBy} ${sortStrategy} ${nullQuery}`;
+    const query = Object.values(this.objQuery).filter(item => Boolean(item)).join(" ");
     this.soqlQuery = query;
   }
-    
+
   // handleQueryClick() {
   // let query = `SELECT ${this.selectFieldName} FROM ${this.selectObjName}`;
   // this.template.querySelector("lightning-textarea").value = query;
   // }
   handleFilteringField(event) {
     this.selectFilteringField = event.detail.value;
-    // const isWhere = this.soqlQuery.includes("WHERE");
-    // const { select, from, orderBy, sortStrategy, nullQuery } = this.objQuery;
-    // if (isWhere) {
-    //   const query = `${select} ${from} ${orderBy} ${sortStrategy} ${nullQuery}`;
-    //   this.soqlQuery = query;
-    // }
+    const query = Object.values(this.objQuery).filter(item => Boolean(item)).join(" ");
+    this.soqlQuery = query;
   }
   handleFilteringOperation(event) {
     this.selectFilteringOperation = event.detail.value;
+    const isWhere = this.soqlQuery.includes("WHERE");
+    if (isWhere) {
+      const query = Object.values(this.objQuery).join(" ");
+      this.soqlQuery = query;
+    }
   }
 
   handleSortingStrategy(event) {
     this.selectSortingStrategy = event.detail.value;
     const isOrderBy = this.soqlQuery.includes("ORDER BY");
-    const { select, from, orderBy, sortStrategy, nullQuery } = this.objQuery;
+
     if (isOrderBy) {
-      const query = `${select} ${from} ${orderBy} ${sortStrategy} ${nullQuery}`;
+      const query = Object.values(this.objQuery).filter(item => Boolean(item)).join(" ");
       this.soqlQuery = query;
     }
   }
 
   handleNull(event) {
     this.selectNull = event.detail.value;
-    const { select, from, orderBy, sortStrategy, nullQuery } = this.objQuery;
-    const query = `${select} ${from} ${orderBy} ${sortStrategy} ${nullQuery}`;
+    let query = '';
+    const { select, from, orderBy, sortStrategy, input, nullQuery } = this.objQuery;
+    if (!input) {
+      query = `${select} ${from} ${orderBy} ${sortStrategy} ${nullQuery}`;
+      this.soqlQuery = query;
+      console.log("input!", query);
+      return;
+    }
+    query = Object.values(this.objQuery).join(" ");
     this.soqlQuery = query;
   }
-
+  
   handleFilterValueChange(event) {
     this.selectFilterValue = event.detail.value;
-    console.log("this.objQuery;", Object.values(this.objQuery));
-    // const { select, from, orderBy, sortStrategy, nullQuery, where, operation   } = this.objQuery;
-  //   let query = "";
-  //   // let query = `
-  //   const SELECT = "SELECT " + this.selectFieldName;
-  //   const FROM = "FROM " + this.selectObjName;
-  //   const WHERE =
-  //     "WHERE " +
-  //     this.selectFilteringField +
-  //     " " +
-  //     this.selectFilteringOperation +
-  //     " " +
-  //     this.selectFilterValue;
-
-  //   if (!this.selectFilterValue) {
-  //     query = `${SELECT} ${FROM}`;
-  //   } else {
-  //     query = `${SELECT} ${FROM} ${WHERE}`;
-  //   }
-  //   this.template.querySelector("lightning-textarea").value = query;
-  // }
-}
+    const query = Object.values(this.objQuery).filter(item => Boolean(item)).join(" ");
+    this.soqlQuery = query;
+  }
 }
